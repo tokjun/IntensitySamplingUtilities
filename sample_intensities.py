dd8db7d3ecb494530251b7612c4731e481b7fd0d
Junichi Tokuda <tokuda@bwh.harvard.edu>
2022-07-26 23:56:08 -0400

    Add the first line to call a python interpreter.

--- sample_intensities.py
+++ sample_intensities.py
@@ -1,27 +1,4 @@
-#
-# Script to sample intensity values in ROIs defined by a label map
-# 
-# Usage: On the Slicer's Python interactor, 
-#
-#   >>> execfile('/path/to/script/IntensitySampling.py')
-# or, if execfile() is not available,
-#   >>> exec(open('/path/to/script/IntensitySampling.py').read())
-# then
-#   >>> IntensitySampling(imageListFile, sourceDir, outputFile)
-#      
-# Arguements:
-#
-#   'imageListFile'
-#      The path to a file that contains the list of input image files.
-#      The first line must be the name of the label map, whereas the rest
-#      are the names of image files to be sampled. One file per line.
-#
-#   'sourceDir'
-#      The path to the folder that contains the image files.
-#
-#   'outputFile'
-#      The path to the output file.
-#
+#!/usr/bin/env python3
 
 import argparse, sys, shutil, os, logging
 import numpy
@@ -53,11 +30,9 @@ def sampleIntensity(imageListFile, sourceDir, outputFile):
 
     ### Get a list of parameters (i.e., TI) and sort
     params = list(imageDict.keys())        # This is a string array
-    print (params)
     params_num = [float(x) for x in params]  # Convert to a numeric array
     params_num, params = zip (*sorted(zip(params_num,params))) # Sort by params_num
     params = list(params)
-    print (params)
 
     for param in params:
         path = sourceDir + '/' + imageDict[param]
